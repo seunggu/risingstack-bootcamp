e537b598e2474bfa2266ccce689380f20415fdc3
seunggu <imaster0209@naver.com>
2017-09-07 14:49:51 +0900

    Step 1: initial

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const logger = require('winston');
+const semver = require('semver');
+const pkg = require('./package.json');
+
+// validate Node version requirement
+const runtime = {
+  expected: semver.validRange(pkg.engines.node),
+  actual: semver.valid(process.version),
+};
+
+const valid = semver.satisfies(runtime.actual, runtime.expected);
+if (!valid) {
+  throw new Error(`Expected Node.js version ${runtime.expected}, but found v${runtime.actual}. ` +
+    'Please update or change your runtime!');
+}
+
+// configure logger
+logger.default.transports.console.colorize = true;
+logger.default.transports.console.timstamp = true;
+logger.default.transports.console.prettyPrint = true;
+
+// start process
+logger.info('Starting web process', { pid: process.pid });
+
+require('./web');
