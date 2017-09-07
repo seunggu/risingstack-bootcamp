1502c78018866cfa9f8eeb1cf49c52864dbac0bd
seunggu <imaster0209@naver.com>
2017-09-07 18:45:48 +0900

    Step 4: initial

---
+++ models/contribution/contribution.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const db = require('../db');
+
+const tableName = '...';
+
+async function insert(param) {}
+async function read(param) {}
+async function insertOrReplace(param) {}
+
+module.exports = {
+  tableName,
+  insert,
+  insertOrReplace,
+  read,
+};
---
+++ models/contribution/contribution.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const db = require('../db');
+const Contribution = require('./contribution');
+
+describe('Contribution', () => {
+  beforeEach(async () => {});
+
+  afterEach(async () => {});
+
+  describe('.insert', () => {
+
+  });
+
+  describe('.insertOrReplace', () => {
+
+  });
+});
