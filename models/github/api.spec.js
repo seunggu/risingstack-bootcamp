f619bd420d0e041542804237c6b673c0a05c770c
seunggu <imaster0209@naver.com>
2017-09-07 15:39:55 +0900

    Step 2: initial

---
+++ models/github/api.spec.js
@@ -0,0 +1,9 @@
+const nock = require('nock');
+const { expect } = require('chai');
+
+const api = require('./api');
+
+describe('Github API', () => {
+  it('should search repositories');
+  it('should get contributors');
+})
